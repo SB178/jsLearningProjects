f34a12a210e11ea456249f5cccf7ee7a33159c7c
Subhrajyoti Bhowmick <46382392+SB178@users.noreply.github.com>
2022-02-20 01:23:00 +0530

    Random Color

---
+++ Random Color/script.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('#body');
+let randColor = () => { 
+	let r = Math.random()*255;
+	let g = Math.random()*255;
+	let b = Math.random()*255;
+	const h1 = document.querySelector('h1');
+	const h4 = document.querySelector('h4');
+	if(r<150 || g <150 || b <150 ) { 
+		h1.style.color = 'white';
+		h4.style.color = 'white';
+	}
+	else { 
+		h1.style.color = 'black';
+		h4.style.color = 'black';
+	}
+	return `rgb(${r},${g},${b})`;
+}
+
+body.addEventListener('click',()=>(body.style.backgroundColor = randColor()));
