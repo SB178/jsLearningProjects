af336cbac78506942cd0759e6afa8417100b5106
S-B-178 <46382392+S-B-178@users.noreply.github.com>
2022-02-20 12:59:02 +0530

    Code optimization Random Color

--- Random Color/script.js
+++ Random Color/script.js
@@ -3,15 +3,19 @@ let randColor = () => {
 	let r = Math.random()*255;
 	let g = Math.random()*255;
 	let b = Math.random()*255;
-	const h1 = document.querySelector('h1');
-	const h4 = document.querySelector('h4');
+	const allText = document.querySelectorAll('.text');
 	if(r<150 || g <150 || b <150 ) { 
-		h1.style.color = 'white';
-		h4.style.color = 'white';
+		for(let text of allText)
+		{
+			text.style.color = 'white';
+		}
 	}
+	
 	else { 
-		h1.style.color = 'black';
-		h4.style.color = 'black';
+		for(let text of allText)
+		{
+			text.style.color = 'black';
+		}
 	}
 	return `rgb(${r},${g},${b})`;
 }
